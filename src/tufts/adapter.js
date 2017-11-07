7074e3ead41bd1dd6ccd87bfb11cab8494b62bd8
Bridget Almas <balmsa@gmail.com>
2017-11-07 14:00:55 -0500

    applying standard coding styles

--- src/tufts/adapter.js
+++ src/tufts/adapter.js
@@ -1,52 +1,50 @@
-import TuftsLatinData from './lang/latin';
-import * as Models from 'alpheios-data-models';
-import WordTestData from '../../tests/data/test-data';
+import TuftsLatinData from './lang/latin'
+import * as Models from 'alpheios-data-models'
+import WordTestData from '../../tests/data/test-data'
 
 class TuftsAdapter {
-    constructor({engine=null,url=null}) {
-      let latin_code = TuftsLatinData.language.toCode();
-      this[latin_code] = TuftsLatinData;
-      //this[Lib.languages.greek] = TuftsGreekData;
-      //this.langMap = new Map([['lat', TuftsLatinData]]);
-      //this.langMap = new Lib.Importer().map('lat', Lib.languages.latin).map('grc', Lib.languages.greek);
-      this.langMap = new Models.FeatureImporter().map('lat', latin_code);
-      this.engineLookup = engine;
-      this.url = url;
-      return this;
-    }
+  constructor ({engine = null, url = null}) {
+    let latinCode = TuftsLatinData.language.toCode()
+    this[latinCode] = TuftsLatinData
+      // this[Lib.languages.greek] = TuftsGreekData;
+      // this.langMap = new Map([['lat', TuftsLatinData]]);
+      // this.langMap = new Lib.Importer().map('lat', Lib.languages.latin).map('grc', Lib.languages.greek);
+    this.langMap = new Models.FeatureImporter().map('lat', latinCode)
+    this.engineLookup = engine
+    this.url = url
+    return this
+  }
 
     // Not implemented yet
-    fetch(lang, word) {
-      let engine = this.engineLookup[lang];
-      let url = this.url.replace('r_WORD',word).replace('r_ENGINE',engine).replace('r_LANG',lang);
-      return new Promise((resolve,reject) =>  {
-        fetch(url).then(
-          function(response) {
-            let json = response.json();
-            resolve(json);
+  fetch (lang, word) {
+    let engine = this.engineLookup[lang]
+    let url = this.url.replace('r_WORD', word).replace('r_ENGINE', engine).replace('r_LANG', lang)
+    return new Promise((resolve, reject) => {
+      window.fetch(url).then(
+          function (response) {
+            let json = response.json()
+            resolve(json)
           }
         ).catch((error) => {
-            reject(error);
-          }
+          reject(error)
+        }
         )
+    })
+  }
 
-      });
-    }
-
-    fetchTestData(lang, word) {
-        return new Promise((resolve, reject) => {
-            try {
-                let wordData = new WordTestData().get(word);
-                console.log(wordData);
-                let json = JSON.parse(wordData);
-                resolve(json);
-            }
-            catch (error) {
+  fetchTestData (lang, word) {
+    return new Promise((resolve, reject) => {
+      try {
+        let wordData = new WordTestData().get(word)
+        console.log(wordData)
+        let json = JSON.parse(wordData)
+        resolve(json)
+      } catch (error) {
                 // Word is not found in test data
-                reject(error);
-            }
-        });
-    }
+        reject(error)
+      }
+    })
+  }
 
     /**
      * A function that maps a morphological service's specific data types and values into an inflection library standard.
@@ -54,83 +52,83 @@ class TuftsAdapter {
      * @param {object} targetWord - the target of the analysis
      * @returns {Homonym} A library standard Homonym object.
      */
-    transform (jsonObj,targetWord) {
-        "use strict";
-        let lexemes = [];
-        let annotationBody = jsonObj.RDF.Annotation.Body;
-        if (!Array.isArray(annotationBody)) {
+  transform (jsonObj, targetWord) {
+    'use strict'
+    let lexemes = []
+    let annotationBody = jsonObj.RDF.Annotation.Body
+    if (!Array.isArray(annotationBody)) {
             /*
             If only one lexeme is returned, Annotation Body will not be an array but rather a single object.
             Let's convert it to an array so we can work with it in the same way no matter what format it is.
              */
-            annotationBody = [annotationBody];
-        }
-        for (let lexeme of annotationBody) {
+      annotationBody = [annotationBody]
+    }
+    for (let lexeme of annotationBody) {
             // Get importer based on the language
-            let language = this.langMap.get(lexeme.rest.entry.dict.hdwd.lang);
-            let lemma = new Models.Lemma(lexeme.rest.entry.dict.hdwd.$, language);
-            let meaning = lexeme.rest.entry.mean ? lexeme.rest.entry.mean.$ : "";
+      let language = this.langMap.get(lexeme.rest.entry.dict.hdwd.lang)
+      let lemma = new Models.Lemma(lexeme.rest.entry.dict.hdwd.$, language)
+      let meaning = lexeme.rest.entry.mean ? lexeme.rest.entry.mean.$ : ''
 
-            let inflections = [];
-            let inflectionsJSON = lexeme.rest.entry.infl;
-            if (!Array.isArray(inflectionsJSON)) {
+      let inflections = []
+      let inflectionsJSON = lexeme.rest.entry.infl
+      if (!Array.isArray(inflectionsJSON)) {
                 // If only one inflection returned, it is a single object, not an array of objects. Convert it to an array for uniformity.
-                inflectionsJSON = [inflectionsJSON];
-            }
-            for (let inflectionJSON of inflectionsJSON) {
-                let inflection = new Models.Inflection(inflectionJSON.term.stem.$, this[language].language.toCode());
-                if (inflectionJSON.term.suff) {
+        inflectionsJSON = [inflectionsJSON]
+      }
+      for (let inflectionJSON of inflectionsJSON) {
+        let inflection = new Models.Inflection(inflectionJSON.term.stem.$, this[language].language.toCode())
+        if (inflectionJSON.term.suff) {
                     // Set suffix if provided by a morphological analyzer
-                    inflection.suffix = inflectionJSON.term.suff.$;
-                }
+          inflection.suffix = inflectionJSON.term.suff.$
+        }
 
                 // Parse whatever grammatical features we're interested in
-                if (inflectionJSON.pofs) {
-                    inflection.feature = this[language][Models.Feature.types.part].get(inflectionJSON.pofs.$);
-                }
-
-                if (inflectionJSON.case) {
-                    inflection.feature = this[language][Models.Feature.types.grmCase].get(inflectionJSON.case.$);
-                }
-
-                if (inflectionJSON.decl) {
-                    inflection.feature = this[language][Models.Feature.types.declension].get(inflectionJSON.decl.$);
-                }
-
-                if (inflectionJSON.num) {
-                    inflection.feature = this[language][Models.Feature.types.number].get(inflectionJSON.num.$);
-                }
-
-                if (inflectionJSON.gend) {
-                    inflection.feature = this[language][Models.Feature.types.gender].get(inflectionJSON.gend.$);
-                }
-
-                if (inflectionJSON.conj) {
-                    inflection.feature = this[language][Models.Feature.types.conjugation].get(inflectionJSON.conj.$);
-                }
-
-                if (inflectionJSON.tense) {
-                    inflection.feature = this[language][Models.Feature.types.tense].get(inflectionJSON.tense.$);
-                }
-
-                if (inflectionJSON.voice) {
-                    inflection.feature = this[language][Models.Feature.types.voice].get(inflectionJSON.voice.$);
-                }
-
-                if (inflectionJSON.mood) {
-                    inflection.feature = this[language][Models.Feature.types.mood].get(inflectionJSON.mood.$);
-                }
-
-                if (inflectionJSON.pers) {
-                    inflection.feature = this[language][Models.Feature.types.person].get(inflectionJSON.pers.$);
-                }
-
-                inflections.push(inflection);
-            }
-            lexemes.push(new Models.Lexeme(lemma, inflections,meaning));
+        if (inflectionJSON.pofs) {
+          inflection.feature = this[language][Models.Feature.types.part].get(inflectionJSON.pofs.$)
         }
-        return new Models.Homonym(lexemes,targetWord);
+
+        if (inflectionJSON.case) {
+          inflection.feature = this[language][Models.Feature.types.grmCase].get(inflectionJSON.case.$)
+        }
+
+        if (inflectionJSON.decl) {
+          inflection.feature = this[language][Models.Feature.types.declension].get(inflectionJSON.decl.$)
+        }
+
+        if (inflectionJSON.num) {
+          inflection.feature = this[language][Models.Feature.types.number].get(inflectionJSON.num.$)
+        }
+
+        if (inflectionJSON.gend) {
+          inflection.feature = this[language][Models.Feature.types.gender].get(inflectionJSON.gend.$)
+        }
+
+        if (inflectionJSON.conj) {
+          inflection.feature = this[language][Models.Feature.types.conjugation].get(inflectionJSON.conj.$)
+        }
+
+        if (inflectionJSON.tense) {
+          inflection.feature = this[language][Models.Feature.types.tense].get(inflectionJSON.tense.$)
+        }
+
+        if (inflectionJSON.voice) {
+          inflection.feature = this[language][Models.Feature.types.voice].get(inflectionJSON.voice.$)
+        }
+
+        if (inflectionJSON.mood) {
+          inflection.feature = this[language][Models.Feature.types.mood].get(inflectionJSON.mood.$)
+        }
+
+        if (inflectionJSON.pers) {
+          inflection.feature = this[language][Models.Feature.types.person].get(inflectionJSON.pers.$)
+        }
+
+        inflections.push(inflection)
+      }
+      lexemes.push(new Models.Lexeme(lemma, inflections, meaning))
     }
+    return new Models.Homonym(lexemes, targetWord)
+  }
 }
 
-export default TuftsAdapter;
+export default TuftsAdapter
--- tests/tufts_adapter.test.js
+++ tests/tufts_adapter.test.js
@@ -1,30 +1,28 @@
-"use strict";
-import TuftsAdapter from '../src/tufts/adapter.js';
-import * as Models from 'alpheios-data-models';
+/* eslint-env jest */
+'use strict'
+import TuftsAdapter from '../src/tufts/adapter.js'
+import * as Models from 'alpheios-data-models'
 
 describe('TuftsAdapter object', () => {
+  let adapter, mare
 
-    let adapter, mare;
+  beforeAll(() => {
+    adapter = new TuftsAdapter({})
+    mare = require('./data/latin_noun_adj_mare.json')
+  })
 
-    beforeAll(() => {
-      adapter  = new TuftsAdapter();
-      mare = require('./data/latin_noun_adj_mare.json');
-
-    });
-
-    test('we adapted mare properly', () => {
-      let homonym = adapter.transform(mare);
-      console.log(Array.isArray(homonym.lexemes));
-      expect(homonym.lexemes.length).toEqual(3);
-      let noun_mare = homonym.lexemes.filter(l => l.lemma.word === 'mare');
-      expect(noun_mare.length).toEqual(1);
-      let noun_marum = homonym.lexemes.filter(l => l.lemma.word === 'marum');
-      expect(noun_marum.length).toEqual(1);
-      let adj_mas = homonym.lexemes.filter(l => l.lemma.word === 'mas');
-      expect(adj_mas.length).toEqual(1);
-      expect(noun_mare[0].inflections.length).toEqual(4);
-      let noun_mare_voc = noun_mare[0].inflections.filter(i => i[Models.Feature.types.grmCase].includes('vocative'));
-      expect(noun_mare_voc.length).toEqual(1);
-    });
-
-});
+  test('we adapted mare properly', () => {
+    let homonym = adapter.transform(mare)
+    console.log(Array.isArray(homonym.lexemes))
+    expect(homonym.lexemes.length).toEqual(3)
+    let nounMare = homonym.lexemes.filter(l => l.lemma.word === 'mare')
+    expect(nounMare.length).toEqual(1)
+    let nounMarum = homonym.lexemes.filter(l => l.lemma.word === 'marum')
+    expect(nounMarum.length).toEqual(1)
+    let adjMas = homonym.lexemes.filter(l => l.lemma.word === 'mas')
+    expect(adjMas.length).toEqual(1)
+    expect(nounMare[0].inflections.length).toEqual(4)
+    let nounMareVoc = nounMare[0].inflections.filter(i => i[Models.Feature.types.grmCase].includes('vocative'))
+    expect(nounMareVoc.length).toEqual(1)
+  })
+})
